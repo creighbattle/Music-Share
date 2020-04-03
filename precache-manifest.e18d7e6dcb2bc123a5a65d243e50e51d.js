9c29e05888e563a6a34d56ac8b9020d0fbca6363
Creigh Battle <creighbattle@Creighs-MacBook-Air.local>
2020-04-02 22:30:52 -0400

    Updates

---
+++ precache-manifest.e18d7e6dcb2bc123a5a65d243e50e51d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9e168f285daa8f974c93c4365527d1",
+    "url": "/Music-Share/index.html"
+  },
+  {
+    "revision": "8b3541463169931a4796",
+    "url": "/Music-Share/static/css/main.20440482.chunk.css"
+  },
+  {
+    "revision": "452103e3eed9ddb92109",
+    "url": "/Music-Share/static/js/2.8030ed11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Music-Share/static/js/2.8030ed11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3541463169931a4796",
+    "url": "/Music-Share/static/js/main.1bdd2bad.chunk.js"
+  },
+  {
+    "revision": "c33a35608a3f194fe2c6",
+    "url": "/Music-Share/static/js/runtime-main.f519191e.js"
+  }
+]);
